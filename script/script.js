9d2b984128f1638f525476c26a90c3c3514d2d25
anasmuostafa <anasmuostafa@gmail.com>
2023-04-04 10:14:10 +0200

    creating the winning function
    each row or column or diagonal

--- script/script.js
+++ script/script.js
@@ -10,7 +10,12 @@ const game = (player1, player2, level) => {
   var lastMove = 2; /*means that player 1 will start*/
   let grid = level + 2;
   let gameBoard = new Array(grid * grid + 1);
-  const result = () => {};
+  const result = () => {
+    if (isDraw) return 0;
+    else if (isWinner(player1.symbolNum)) return 1;
+    else if (isWinner(player2.symbolNum)) return 2;
+    else return 2;
+  };
   const makeAMove = (playerMove) => {
     if (playerMove <= grid * grid && !gameBoard[playerMove]) {
       /* check if clicked place is empty */
@@ -18,13 +23,6 @@ const game = (player1, player2, level) => {
         move(player2, playerMove);
       } else {
         move(player1, playerMove);
-      }
-      console.log(gameBoard);
-
-      if (isWinner(player1.getSymbolNum())) {
-        console.log("player1 wins");
-      } else if (isWinner(player2.getSymbolNum())) {
-        console.log("player2 wins");
       }
     }
   };
@@ -35,9 +33,52 @@ const game = (player1, player2, level) => {
   };
   const isDraw = () => {};
   const isWinner = (symbolNum) => {
-
+    return (
+      isDiagonalWinning(symbolNum) ||
+      isColumnsWinning(symbolNum) ||
+      isRowsWinning(symbolNum)
+    );
+  };
+  const isDiagonalWinning = (symbolNum) => {
+    let count1 = 0;
+    let count2 = 0;
+    for (let i = 0; i < grid; i++) {
+      if (gameBoard[1 + grid * i + i] == symbolNum) {
+        count1++;
+      }
+      if (gameBoard[grid + grid * i - i] == symbolNum) {
+        count2++;
+      }
+    }
+    if (count1 == grid || count2 == grid) {
+      return true;
+    } else return false;
+  };
+  const isColumnsWinning = (symbolNum) => {
+    for (let i = 1; i <= grid; i++) {
+      let count = 0;
+      for (let j = i; j < grid * grid; j += grid) {
+        if (gameBoard[j] == symbolNum) {
+          count++;
+        }
+      }
+      if (count == grid) return true;
+    }
+    return false;
   };
 
+  const isRowsWinning = (symbolNum) => {
+    for (let i = 1; i < grid * grid; i += grid) {
+      let count = 0;
+      for (let j = 0; j < grid; j++) {
+        if (gameBoard[i + j] == symbolNum) {
+          count++;
+        }
+      }
+      if (count == grid) return true;
+    }
+    return false;
+  };
   const clearGrid = () => {
     gameBoard = new Array(grid * grid + 1);
   };
