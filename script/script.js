3db422c2ca1f0c22e12cbd0e27e6edc556129200
anasmuostafa <anasmuostafa@gmail.com>
2023-04-04 10:08:26 +0200

    function to make a move

--- script/script.js
+++ script/script.js
@@ -10,11 +10,28 @@ const game = (player1, player2, level) => {
   var lastMove = 2; /*means that player 1 will start*/
   let grid = level + 2;
   let gameBoard = new Array(grid * grid + 1);
-  const result = () => {
-  };
+  const result = () => {};
   const makeAMove = (playerMove) => {
+    if (playerMove <= grid * grid && !gameBoard[playerMove]) {
+      /* check if clicked place is empty */
+      if (lastMove === 1) {
+        move(player2, playerMove);
+      } else {
+        move(player1, playerMove);
+      }
+      console.log(gameBoard);
+
+      if (isWinner(player1.getSymbolNum())) {
+        console.log("player1 wins");
+      } else if (isWinner(player2.getSymbolNum())) {
+        console.log("player2 wins");
+      }
+    }
   };
   const move = (player, move) => {
+    gameBoard[move] = player.getSymbolNum();
+    if (lastMove == 1) lastMove = 2;
+    else lastMove = 1;
   };
   const isDraw = () => {};
   const isWinner = (symbolNum) => {
